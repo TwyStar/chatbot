954da6d2cf47267e0eb158d5156b73482cfaf086
TwyStar <zoli_94@windowslive.com>
2023-01-15 21:26:35 +0100

    [dev] chatbot

--- chatbot/ContentView.swift
+++ chatbot/ContentView.swift
@@ -5,12 +5,74 @@
 //  Created by Balaska Zoltán on 2023. 01. 15..
 //
 
+import OpenAISwift
 import SwiftUI
 
+final class ViewModel: ObservableObject{
+    init() {}
+    
+    private var client: OpenAISwift?
+    
+    func setup () {
+        client = OpenAISwift(authToken: "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")
+        
+    }
+    
+    func send (text: String, completion: @escaping(String) -> Void){
+        client?.sendCompletion(with: text,maxTokens: 500, completionHandler: {result in
+            
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+            }
+        })
+        
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading){
+            ForEach(models , id: \.self){ string in
+                Text(string)
+            }
+            Spacer()
+            
+            HStack{
+                TextField("Ide írj...", text: $text)
+                Button("Küld"){
+                    send()
+                    
+                }
+            }
+        }
+        .onAppear{
+            viewModel.setup()
+        }
+        .padding()
+    }
+    
+    func send(){
+        guard text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        models.append("Én: \(text)")
+        viewModel.send(text: text) { respons in
+            DispatchQueue.main.async {
+                self.models.append("ChatBot mondja: "+respons)
+                self.text = ""
+            }
+        }
     }
 }
 
