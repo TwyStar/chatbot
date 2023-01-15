d12f72fd56b74aa575e8a9cc06028ae1b5c52c1f
TwyStar <zoli_94@windowslive.com>
2023-01-15 20:22:16 +0100

    Initial Commit

---
+++ chatbot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chatbot
+//
+//  Created by Balaska Zoltán on 2023. 01. 15..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chatbot/chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbotApp.swift
+//  chatbot
+//
+//  Created by Balaska Zoltán on 2023. 01. 15..
+//
+
+import SwiftUI
+
+@main
+struct chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
